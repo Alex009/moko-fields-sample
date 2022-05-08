29b0eee200b1a87ac12b9c179831db6313bfaf05
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2022-05-08 13:28:00 +0700

    field validation usage

--- shared/src/commonMain/kotlin/ru/alex009/moko/fields/myapplication/LoginViewModel.kt
+++ shared/src/commonMain/kotlin/ru/alex009/moko/fields/myapplication/LoginViewModel.kt
@@ -1,20 +1,20 @@
 package ru.alex009.moko.fields.myapplication
 
 import dev.icerock.moko.fields.FormField
-import dev.icerock.moko.fields.liveBlock
 import dev.icerock.moko.fields.validate
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import dev.icerock.moko.resources.desc.StringDesc
 import dev.icerock.moko.resources.desc.desc
+import dev.icerock.moko.validations.fieldValidation
+import dev.icerock.moko.validations.notBlank
 import ru.alex009.moko.fields.MR
 
 class LoginViewModel : ViewModel() {
     // region Login LiveData
     val login: FormField<String, StringDesc> = FormField(
         initialValue = "",
-        validation = liveBlock {
-            if (it.isBlank()) MR.strings.login_blank_error.desc()
-            else null
+        validation = fieldValidation {
+            notBlank(MR.strings.login_blank_error.desc())
         }
     )
     // endregion
@@ -22,9 +22,8 @@ class LoginViewModel : ViewModel() {
     // region Password LiveData
     val password: FormField<String, StringDesc> = FormField(
         initialValue = "",
-        validation = liveBlock {
-            if (it.isBlank()) MR.strings.password_blank_error.desc()
-            else null
+        validation = fieldValidation {
+            notBlank(MR.strings.password_blank_error.desc())
         }
     )
     // endregion
